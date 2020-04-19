e29355a4b07ab6be08ef38c565f620a13486aa9f
alfianjau <alfianjauhari@gmail.com>
2020-04-19 14:02:23 +0700

    add dynamic header and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,27 @@
-import React, { Component } from "react";
-import { Link } from "gatsby";
-import UserLinks from "../UserLinks/UserLinks";
-import "./Footer.css";
+/** @jsx jsx */
+
+import { jsx } from 'theme-ui'
+import { Component } from 'react'
+import { Link } from 'gatsby'
+import UserLinks from '../UserLinks/UserLinks'
+import './Footer.css'
 
 class Footer extends Component {
   render() {
-    const { config } = this.props;
-    const url = config.siteRss;
-    const { copyright } = config;
+    const { config } = this.props
+    const url = config.siteRss
+    const { copyright } = config
     if (!copyright) {
-      return null;
+      return null
     }
     return (
-      <footer className="footer">
+      <footer
+        className="footer"
+        sx={{
+          fontSize: 1,
+          variant: 'styles.footer',
+        }}
+      >
         <UserLinks config={config} labeled />
         <div className="notice-container">
           <h4>{copyright}</h4>
@@ -21,16 +30,91 @@ class Footer extends Component {
             <button>Subscribe</button>
           </Link>
           <h4>
-            Based on{" "}
+            Based on{' '}
             <a href="https://github.com/Vagr9K/gatsby-advanced-starter">
               Gatsby Advanced Starter
             </a>
             .
           </h4>
         </div>
+        <div
+          sx={{
+            display: 'grid',
+            gridTemplateRows: 'repeat(4, 32px)',
+            gridTemplateColumns: ['repeat(2, 1fr)', 'repeat(4, 1fr)'],
+            gridAutoFlow: 'column',
+            px: 2,
+            py: 4,
+          }}
+        >
+          <Link to="/" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Home
+          </Link>
+          <Link to="/work" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Work
+          </Link>
+          <Link to="/blog" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Blog
+          </Link>
+          <Link to="/about" sx={{ variant: 'styles.navlink', p: 2 }}>
+            About
+          </Link>
+          <Link to="/products" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Products
+          </Link>
+          <Link to="/community" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Community
+          </Link>
+          <Link to="/support" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Support
+          </Link>
+          <Link to="/contact" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Contact
+          </Link>
+          <Link to="/support" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Support
+          </Link>
+          <Link to="/products" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Products
+          </Link>
+          <Link to="/contact" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Contact
+          </Link>
+          <Link to="/community" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Community
+          </Link>
+          <Link to="/products" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Products
+          </Link>
+          <Link to="/community" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Community
+          </Link>
+          <Link to="/support" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Support
+          </Link>
+          <Link to="/contact" sx={{ variant: 'styles.navlink', p: 2 }}>
+            Contact
+          </Link>
+        </div>
+        <div
+          sx={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            p: 3,
+          }}
+        >
+          <Link to="/privacy-policy" sx={{ color: 'inherit' }}>
+            Privacy Policy
+          </Link>
+          <div sx={{ mx: 1 }} />
+          <Link to="/terms-of-use" sx={{ color: 'inherit' }}>
+            Terms of Use
+          </Link>
+          <div sx={{ mx: 1 }} />© 2019 Jane Doe
+        </div>
       </footer>
-    );
+    )
   }
 }
 
-export default Footer;
+export default Footer
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
-import React, { Component } from 'react'
+/** @jsx jsx */
+
+import { jsx } from 'theme-ui'
+import { Component } from 'react'
 import { Link } from 'gatsby'
 import UserLinks from '../UserLinks/UserLinks'
+
 // import './Footer.css'
 
 class Footer extends Component {
@@ -12,83 +16,82 @@ class Footer extends Component {
       return null
     }
     return (
-      <header className="header">
+      <header
+        className="header"
+        sx={{
+          py: 4,
+          variant: 'styles.header',
+        }}
+      >
         <UserLinks config={config} labeled />
-        <header
+        <div
           sx={{
-            py: 4,
-            variant: 'styles.header',
+            display: 'flex',
+            justifyContent: 'center',
+            mb: 3,
           }}
         >
-          <div
+          <Link to="/" title="Home">
+            <img
+              alt="UI Logo"
+              src="https://contrast.now.sh/cff/40f?size=48&fontSize=2&baseline=2&fontWeight=900&radius=32&text=UI"
+            />
+            <span
+              sx={{
+                position: 'absolute',
+                width: 1,
+                height: 1,
+                overflow: 'hidden',
+                top: -9999,
+              }}
+            >
+              Home
+            </span>
+          </Link>
+        </div>
+        <div
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <Link
+            to="/work"
             sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              mb: 3,
+              variant: 'styles.navlink',
+              p: 2,
             }}
           >
-            <Link to="/" title="Home">
-              <img
-                alt="UI Logo"
-                src="https://contrast.now.sh/cff/40f?size=48&fontSize=2&baseline=2&fontWeight=900&radius=32&text=UI"
-              />
-              <span
-                sx={{
-                  position: 'absolute',
-                  width: 1,
-                  height: 1,
-                  overflow: 'hidden',
-                  top: -9999,
-                }}
-              >
-                Home
-              </span>
-            </Link>
-          </div>
-          <div
+            Work
+          </Link>
+          <Link
+            to="/blog"
             sx={{
-              display: 'flex',
-              justifyContent: 'center',
+              variant: 'styles.navlink',
+              p: 2,
             }}
           >
-            <Link
-              to="/work"
-              sx={{
-                variant: 'styles.navlink',
-                p: 2,
-              }}
-            >
-              Work
-            </Link>
-            <Link
-              to="/blog"
-              sx={{
-                variant: 'styles.navlink',
-                p: 2,
-              }}
-            >
-              Blog
-            </Link>
-            <Link
-              to="/about"
-              sx={{
-                variant: 'styles.navlink',
-                p: 2,
-              }}
-            >
-              About
-            </Link>
-            <Link
-              to="/contact"
-              sx={{
-                variant: 'styles.navlink',
-                p: 2,
-              }}
-            >
-              Contact
-            </Link>
-          </div>
-        </header>
+            Blog
+          </Link>
+          <Link
+            to="/about"
+            sx={{
+              variant: 'styles.navlink',
+              p: 2,
+            }}
+          >
+            About
+          </Link>
+          <Link
+            to="/contact"
+            sx={{
+              variant: 'styles.navlink',
+              p: 2,
+            }}
+          >
+            Contact
+          </Link>
+        </div>
       </header>
     )
   }
