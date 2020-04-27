b7aff5d03f96eb7c5f4a528098208d531144e506
alfianjau <alfianjauhari@gmail.com>
2020-04-28 00:55:08 +0700

    resetup some css config due postcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,10 @@ module.exports = {
         // develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        ignore: [
+          '@brainhubeu/react-carousel/lib/style.css',
+          'react-tabs/style/react-tabs.css',
+        ], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
